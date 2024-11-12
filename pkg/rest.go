7926ddc61928cb818f3cadbddc5d74e09c679735
Korrigon <korrig@goletan.tech>
2024-11-12 11:54:48 +0100

    - fix setup of REST Server

--- pkg/rest.go
+++ pkg/rest.go
@@ -2,18 +2,17 @@
 package rest
 
 import (
-	"fmt"
-
 	"github.com/goletan/api-rest/internal/server"
 	observability "github.com/goletan/observability/pkg"
 	services "github.com/goletan/services/pkg"
+	"go.uber.org/zap"
 )
 
 // NewRESTService creates a new REST service that implements the Goletan service interface.
-func NewRESTService() services.Service {
+func NewRESTService(logger *zap.Logger) services.Service {
 	obs, err := observability.NewObserver()
 	if err != nil {
-		fmt.Errorf("cannot create new observer")
+		logger.Error("Failed to initialize observability", zap.Error(err))
 	}
 
 	return server.NewRESTServer(obs)
